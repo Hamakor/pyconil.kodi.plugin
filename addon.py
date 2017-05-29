0522879768c9b4b70a8227628e3cd6bd9537c189
Amit Aronovitch <aaronovitch@BrightSourceEnergy.com>
2017-05-29 18:39:19 +0300

    empty plugin script that prints the commandline argument to the log

---
+++ addon.py
@@ -0,0 +1,9 @@
+import xbmc
+
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+
+xbmc.log("Hello", xbmc.LOGNOTICE)#xbmc.LOGDEBUG)
+xbmc.log(str(sys.argv), xbmc.LOGNOTICE)
